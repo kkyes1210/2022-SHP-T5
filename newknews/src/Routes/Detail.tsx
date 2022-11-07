471cc22da6600b950179732e8ee416ab65ed988b
AtBHtBt <coasterrrrr@gmail.com>
2022-11-07 16:56:49 +0900

    react + spring 2

---
+++ newknews/src/Routes/Detail.tsx
@@ -0,0 +1,15 @@
+function Detail() {
+  return (
+    <>
+      <h1>test page</h1>
+      <div id="class">
+        <h2>
+          Hellow Wolrd!
+        </h2>
+      </div>
+    </>
+  );
+  
+}
+
+export default Detail;
